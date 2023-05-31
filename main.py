d56e222da04eb18c27f05f1cd7f49afb792f756c
IAmStiven <132297465+IAmStiven@users.noreply.github.com>
2023-05-31 22:44:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="og:image" content="https://cdn.discordapp.com/avatars/295917036335398922/b91342410e6cc08011fe7ca10223a18d.png?size=2048">
     <meta name="og:title" content="Eviltwin">
-    <meta name="og:description" content="Just an evil teen">
+    <meta name="og:description" content="'Pls cumshot on me' - Eviltwin 2010-2023">
     <title>Document</title>
 </head>
 <body>
